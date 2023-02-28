606f8c8e21ecaeb90687e1878956d7148e6238ce
Ochoa21 <xavy125427@gmail.com>
2023-02-28 16:54:01 -0500

    Nya

---
+++ src/juegos.js
@@ -0,0 +1,69 @@
+const juegos = (prefix, pushname) => {
+    return `
+*Comandos De Juegos 👾*
+ 
+${prefix}rankgay
+Conoce a el top de los mas gays del grupo
+
+${prefix}gay
+Conoce que tan gay eres 
+
+${prefix}cuties
+Prueba tu suerte en el medidor de fan de cuties 
+
+${prefix}pendejo
+Conoce que tan pendejo eres
+
+${prefix}rankpendejos
+Conoce a los mas pendejos del grupo
+
+${prefix}ceperos
+Conoce que tan fan eres de cp
+
+${prefix}simps
+Conoce que tan simp eres
+
+${prefix}lucky
+Prueba tu suerte con este gran juego
+
+${prefix}lucky2
+Aca esta el 2 por si el 1 falla
+
+${prefix}bot
+Hazle preguntas al bot 
+
+${prefix}verdad
+Bueno vamo a jugar 
+
+${prefix}reto
+Pide y cumple puta
+
+${prefix}pareja
+Mira con quien te conectas
+
+${prefix}rankgod
+Los mejores del grupo
+
+${prefix}lesbiana
+Mira y luego te sales
+
+${prefix}bisexual
+Asco de juego pero bue
+
+${prefix}hetero
+Aver si eres admin
+
+${prefix}puta
+Y de paso dame sentones
+
+${prefix}ttt
+Juega Tit Tac Toe 
+
+_*En futuras versiones colocare mas juegos :)*_
+     
+_*by Ochoa*_
+`
+
+}
+
+exports.juegos = juegos
